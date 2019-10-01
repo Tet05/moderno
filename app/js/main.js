e4bc26acdbc2f4a7998a3c2ab0e3a9497b649d10
Tet05 <41017699+Tet05@users.noreply.github.com>
2019-10-01 09:56:27 +0300

    added product page and aside

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,18 @@ $( function () {
 
 	} );
 
+
+
+	$( ".js-range-slider" ).ionRangeSlider( {
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	} );
+
+	// .$( '.icon-th-list' ).
+
 	var mixer = mixitup( '.products__inner-box' );
 } );
